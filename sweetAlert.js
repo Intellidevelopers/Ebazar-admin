19edf593ab3de5fe102db96c891a336b678c3984
Intellidevelopers <134317370+Intellidevelopers@users.noreply.github.com>
2023-11-15 07:00:46 +0100

    g

---
+++ sweetAlert.js
@@ -0,0 +1,16 @@
+function confirmDelete() {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: 'You won\'t be able to revert this!',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            // Handle the delete action here
+            Swal.fire('Deleted!', 'Your item has been deleted.', 'success');
+        }
+    });
+}
